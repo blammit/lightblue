eefbe2eb6aee8964cdf2c0b5c4dccaed1e36b7f0
Bea Lam <blammit@gmail.com>
2022-03-10 21:33:22 +1000

    Make new .xcodeproj with XCode 12.5.1

---
+++ src/mac/LightAquaBlue/LightAquaBlue/LightAquaBlue.h
@@ -0,0 +1,16 @@
+//
+//  LightAquaBlue.h
+//  LightAquaBlue
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LightAquaBlue.
+FOUNDATION_EXPORT double LightAquaBlueVersionNumber;
+
+//! Project version string for LightAquaBlue.
+FOUNDATION_EXPORT const unsigned char LightAquaBlueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightAquaBlue/PublicHeader.h>
+
+
